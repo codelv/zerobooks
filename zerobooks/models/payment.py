f7b7b42e5774bf93eae275a0960fe52c00d5da52
frm dstryr <frmdstryr@protonmail.com>
2023-09-01 08:42:17 -0400

    Change loicense to GPL

--- zerobooks/models/payment.py
+++ zerobooks/models/payment.py
@@ -1,7 +1,7 @@
 """
 Copyright (c) 2023, Jairus Martin.
 
-Distributed under the terms of the AGPL v3 License.
+Distributed under the terms of the GPL v3 License.
 
 The full license is in the file LICENSE, distributed with this software.
 """
